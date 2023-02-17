08423b0ec067841e904df4072c9798088cf427d1
key <422943393@qq.com>
2023-02-17 20:06:17 +0800

    init

---
+++ gulimall-ware/src/main/java/com/cocoamu/guliall/ware/GuliallWareApplication.java
@@ -0,0 +1,13 @@
+package com.cocoamu.guliall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliallWareApplication.class, args);
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/cocoamu/guliall/ware/GuliallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocoamu.guliall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
